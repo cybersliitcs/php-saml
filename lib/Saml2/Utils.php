591b686c17af141464d3d5d47a434b81f8f5e274
HasalaS <48043521+HasalaS@users.noreply.github.com>
2020-04-30 17:05:50 +0530

    Update Utils.php

--- lib/Saml2/Utils.php
+++ lib/Saml2/Utils.php
@@ -736,7 +736,7 @@ class OneLogin_Saml2_Utils
      */
     public static function generateUniqueID()
     {
-        return 'ONELOGIN_' . sha1(uniqid((string)mt_rand(), true));
+        return 'ONELOGIN_' . sha512(uniqid((string)random_bytes(), true));
     }
 
     /**
