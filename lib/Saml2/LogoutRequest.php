0a1cb7027631068c44cd6ae00ec575eca5dd74b4
cybersliitcs <64581192+cybersliitcs@users.noreply.github.com>
2020-05-01 14:11:59 +0530

    Update LogoutRequest.php

--- lib/Saml2/LogoutRequest.php
+++ lib/Saml2/LogoutRequest.php
@@ -406,7 +406,11 @@ LOGOUTREQUEST;
                     );
                 }
             }
-	 if (isset($_GET['Signature']))  {
+	 /*edited  lib/saml2/logoutrequest.php*/
+            if(
+  isset( $_GET['SAMLResponse']) && isset( $_GET['nonce']) 
+  && wp_verify_nonce(sanitize_key($_GET['nonce']), 'SAMLResponse_action')
+  )/*End edited*/ {
                 $signatureValid = OneLogin_Saml2_Utils::validateBinarySign("SAMLRequest", $_GET, $idpData, $retrieveParametersFromServer);
                 if (!$signatureValid) {
                     throw new OneLogin_Saml2_ValidationError(
